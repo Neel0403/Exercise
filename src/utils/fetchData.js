cf21901d11aaa9131173d24114e0e19a3ac32e13
Neel0403 <102226333+Neel0403@users.noreply.github.com>
2024-01-20 18:45:54 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,14 +2,14 @@ export const exerciseOptions = {
     method: "GET",
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': "7d9662c098mshd3e16b5ab3737f7p10ceefjsn6a0afae8c657"
+        'X-RapidAPI-Key': REACT_APP_RAPID_API_KEY
     },
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': "7d9662c098mshd3e16b5ab3737f7p10ceefjsn6a0afae8c657",
+    'X-RapidAPI-Key': REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
