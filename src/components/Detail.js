de2e8860ce8db3f260b2909cdc7ceef87203a1e0
neel <neelo2003@gmail.com>
2024-05-03 19:51:46 +0530

    Fix map function

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -34,8 +34,8 @@ const Detail = ({ exerciseDetail }) => {
                     exercises to target your {target}.It will help you
                     improve your mood and gain energy
                 </Typography>
-                {extraDetail.map((item) => (
-                    <Stack key={item.name} direction="row" gap="24px" alignItems="center" >
+                {extraDetail.map((item,index) => (
+                    <Stack key={index} direction="row" gap="24px" alignItems="center" >
                         <Button sx={{ background: "#fff2db", borderRadius: "50%", width: "100px", height: "100px" }}>
                             <img src={item.icon} alt={bodyPart} style={{ width: "50px", height: "50px" }} />
                         </Button>
