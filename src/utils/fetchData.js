0b1323e2f6b3b338ce1009bebb9f6cf8b413593e
neel <neelo2003@gmail.com>
2024-05-03 19:55:24 +0530

    Fix Api key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '7d9662c098mshd3e16b5ab3737f7p10ceefjsn6a0afae8c657',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
